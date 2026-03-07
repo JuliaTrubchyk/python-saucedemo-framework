c62d867060ba67119928b54d8e1519a7572a630b
Yuliia <julia.koshovets@gmail.com>
2026-03-06 16:27:35 -0800

    Add login test using LoginPage page object (POM)

--- tests/ui/test_smoke_open_site.py
+++ tests/ui/test_smoke_open_site.py
@@ -1,3 +1,12 @@
-def test_open_site(driver):
+from pages.login_page import LoginPage
+from selenium.webdriver.common.by import By
+
+
+def test_login_success(driver):
     driver.get("https://www.saucedemo.com/")
-    assert "Swag Labs" in driver.title
+
+    login_page = LoginPage(driver)
+    login_page.login("standard_user", "secret_sauce")
+
+    page_title = driver.find_element(By.CLASS_NAME, "title").text
+    assert page_title == "Products"
