6440ed03ba125d11a3a67fc2a8ad579b2764241b
Yuliia <julia.koshovets@gmail.com>
2026-03-19 14:27:28 -0700

    Refactor negative login tests using pytest parametrize

--- tests/ui/test_login.py
+++ tests/ui/test_login.py
@@ -13,37 +13,19 @@ def test_login_success(driver):
     assert page_title == "Products"
 
 @pytest.mark.regression
-def test_login_invalid_password(driver):
+@pytest.mark.parametrize(
+    "username,password,expected_error",
+    [
+        ("standard_user", "wrong_pwd", "Username and password do not match"),
+        ("standard_user", "", "Password is required"),
+        ("", "secret_sauce", "Username is required"),
+        ("locked_out_user", "secret_sauce", "Sorry, this user has been locked out"),
+    ]
+)
+def test_login_negative_cases(driver, username, password, expected_error):
     login_page = LoginPage(driver)
     login_page.open()
-    login_page.login("standard_user", "wrong_pwd")
+    login_page.login(username, password)
 
     error_message = login_page.get_error_message()
-    assert "Username and password do not match" in error_message
-
-@pytest.mark.regression
-def test_login_empty_password(driver):
-    login_page = LoginPage(driver)
-    login_page.open()
-    login_page.login("standard_user", "")
-
-    error_message = login_page.get_error_message()
-    assert "Password is required" in error_message
-
-@pytest.mark.regression
-def test_login_empty_username(driver):
-    login_page = LoginPage(driver)
-    login_page.open()
-    login_page.login("", "secret_sauce")
-
-    error_message = login_page.get_error_message()
-    assert "Username is required" in error_message
-
-@pytest.mark.regression
-def test_login_locked_out_user(driver):
-    login_page = LoginPage(driver)
-    login_page.open()
-    login_page.login("locked_out_user", "secret_sauce")
-
-    error_message = login_page.get_error_message()
-    assert "Sorry, this user has been locked out" in error_message
+    assert expected_error in error_message
