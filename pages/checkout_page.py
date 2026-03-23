e05762d901539df5f09884b0e051af3fe1619138
Yuliia <julia.koshovets@gmail.com>
2026-03-23 10:00:38 -0700

    Add checkout flow with form submission and navigation test

--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -3,6 +3,18 @@ from pages.base_page import BasePage
 
 class CheckoutPage(BasePage):
     PAGE_TITLE = (By.CLASS_NAME, "title")
+    FIRST_NAME_INPUT = (By.ID, "first-name")
+    LAST_NAME_INPUT = (By.ID, "last-name")
+    POSTAL_CODE_INPUT = (By.ID, "postal-code")
+    CONTINUE_BUTTON = (By.ID, "continue")
 
     def get_page_title(self):
         return self.get_text(self.PAGE_TITLE)
+
+    def fill_checkout_information(self, first_name, last_name, postal_code):
+        self.type(self.FIRST_NAME_INPUT, first_name)
+        self.type(self.LAST_NAME_INPUT, last_name)
+        self.type(self.POSTAL_CODE_INPUT, postal_code)
+
+    def continue_checkout(self):
+        self.click(self.CONTINUE_BUTTON)
--- tests/ui/test_checkout.py
+++ tests/ui/test_checkout.py
@@ -15,3 +15,21 @@ def test_user_can_open_checkout_page_from_cart(driver):
 
     page_title = checkout_page.get_page_title()
     assert page_title == "Checkout: Your Information"
+
+
+@pytest.mark.regression
+def test_user_can_fill_checkout_information_and_continue(driver):
+    login_page = LoginPage(driver)
+    login_page.open()
+
+    inventory_page = login_page.login("standard_user", "secret_sauce")
+    inventory_page.add_backpack_to_cart()
+
+    cart_page = inventory_page.open_cart_page()
+    checkout_page = cart_page.checkout()
+
+    checkout_page.fill_checkout_information("John", "Doe", "12345")
+    checkout_page.continue_checkout()
+
+    page_title = checkout_page.get_page_title()
+    assert page_title == "Checkout: Overview"
